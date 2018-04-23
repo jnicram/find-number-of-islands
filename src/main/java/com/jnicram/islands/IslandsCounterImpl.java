6098adedbb962f131d770449aea3d12613f2facf
Marcin Jasinski <jnicram@gmail.com>
2018-04-23 23:17:31 +0200

    implementation

--- src/main/java/com/jnicram/islands/IslandsCounterImpl.java
+++ src/main/java/com/jnicram/islands/IslandsCounterImpl.java
@@ -2,8 +2,42 @@ package com.jnicram.islands;
 
 public class IslandsCounterImpl implements IslandsCounter {
 
+    private static final int[] ROW_NEIGHBORS = {-1, -1, -1, 0, 0, 1, 1, 1};
+    private static final int[] COL_NEIGHBORS = {-1, 0, 1, -1, 1, -1, 0, 1};
+
     @Override
     public int count(int[][] map) {
-        return -1;
+        int result = 0;
+        if (map.length == 0) return result;
+
+        final int xLength = map.length;
+        final int yLength = map[0].length;
+        
+        boolean visited[][] = new boolean[xLength][yLength];
+        for (int x = 0; x < xLength; ++x) {
+            for (int y = 0; y < yLength; ++y) {
+                if (map[x][y] == 1 && !visited[x][y]) {
+                    dfs(map, x, y, visited);
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean canCellBeIncluded(int[][] map, int row, int col, boolean visited[][]) {
+        final int xLength = map.length;
+        final int yLength = map[0].length;
+
+        return (row >= 0) && (row < xLength) && (col >= 0) && (col < yLength) && (map[row][col] == 1 && !visited[row][col]);
+    }
+
+    private void dfs(int[][] map, int row, int col, boolean visited[][]) {
+        visited[row][col] = true;
+
+        for (int k = 0; k < 8; ++k) {
+            if (canCellBeIncluded(map, row + ROW_NEIGHBORS[k], col + COL_NEIGHBORS[k], visited))
+                dfs(map, row + ROW_NEIGHBORS[k], col + COL_NEIGHBORS[k], visited);
+        }
     }
 }
--- src/test/java/com/jnicram/islands/IslandsCounterImplTest.java
+++ src/test/java/com/jnicram/islands/IslandsCounterImplTest.java
@@ -1,5 +1,78 @@
 package com.jnicram.islands;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class IslandsCounterImplTest {
 
+    private final int[][] map;
+    private final int expectedCount;
+
+    private IslandsCounter classUnderTest;
+
+    public IslandsCounterImplTest(int[][] map, int count) {
+        this.map = map;
+        this.expectedCount = count;
+    }
+
+    @Parameterized.Parameters( name = "{index}: should find {1} island(s)" )
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+                { new int[][] {} , 0 },
+                { new int[][] { { 1 } }, 1},
+                { new int[][] { { 0 } }, 0},
+                { new int[][] {
+                        { 1, 1, 0 },
+                        { 0, 1, 0 },
+                        { 1, 0, 0 }
+                }, 1},
+                { new int[][] {
+                        { 1, 0, 1 },
+                        { 0, 0, 1 },
+                }, 2},
+                { new int[][] {
+                        { 1, 0, 1 },
+                        { 0, 0, 1 },
+                        { 1, 0, 0 }
+                }, 3},
+                { new int[][] {
+                        { 1, 1, 1, 1, 1 },
+                        { 1, 0, 0, 0, 1 },
+                        { 1, 0, 0, 0, 1 },
+                        { 1, 0, 1, 0, 1 },
+                        { 1, 0, 0, 0, 1 }
+                }, 2},
+                { new int[][] {
+                        { 1, 1, 0, 0, 0 },
+                        { 0, 1, 0, 0, 1 },
+                        { 1, 0, 0, 1, 1 },
+                        { 0, 0, 0, 0, 0 },
+                        { 1, 0, 1, 0, 1 }
+                }, 5}
+        };
+        return Arrays.asList(data);
+    }
+
+    @Before
+    public void init() {
+        classUnderTest = new IslandsCounterImpl();
+    }
+
+    @Test
+    public void shouldReturns0WhenEmptyMapGiven() {
+        // given
+
+        // when
+        final int result = classUnderTest.count(map);
+
+        // then
+        Assert.assertEquals(expectedCount, result);
+    }
 }
