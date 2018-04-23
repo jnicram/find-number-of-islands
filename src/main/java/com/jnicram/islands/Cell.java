8f0fb00613fd690bafe838d90541227bd7ba8c85
Marcin Jasinski <jnicram@gmail.com>
2018-04-23 23:40:38 +0200

    refactoring

---
+++ src/main/java/com/jnicram/islands/Cell.java
@@ -0,0 +1,12 @@
+package com.jnicram.islands;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+class Cell {
+
+    private int row;
+    private int col;
+}
--- src/main/java/com/jnicram/islands/IslandsCounterImpl.java
+++ src/main/java/com/jnicram/islands/IslandsCounterImpl.java
@@ -1,5 +1,7 @@
 package com.jnicram.islands;
 
+import java.util.Stack;
+
 public class IslandsCounterImpl implements IslandsCounter {
 
     private static final int[] ROW_NEIGHBORS = {-1, -1, -1, 0, 0, 1, 1, 1};
@@ -17,7 +19,7 @@ public class IslandsCounterImpl implements IslandsCounter {
         for (int x = 0; x < xLength; ++x) {
             for (int y = 0; y < yLength; ++y) {
                 if (map[x][y] == 1 && !visited[x][y]) {
-                    dfs(map, x, y, visited);
+                    dfs(map, new Cell(x, y), visited);
                     ++result;
                 }
             }
@@ -25,19 +27,27 @@ public class IslandsCounterImpl implements IslandsCounter {
         return result;
     }
 
-    private boolean canCellBeIncluded(int[][] map, int row, int col, boolean visited[][]) {
+    private void dfs(int[][] map, Cell cell, boolean visited[][]) {
+        Stack<Cell> stack = new Stack<>();
+        stack.push(cell);
+
+        while (!stack.empty()) {
+            final Cell pop = stack.pop();
+            if (!visited[pop.getRow()][pop.getCol()]) {
+                visited[pop.getRow()][pop.getCol()] = true;
+                for (int k = 0; k < 8; ++k) {
+                    Cell cellToBeVisited = new Cell(pop.getRow() + ROW_NEIGHBORS[k], pop.getCol() + COL_NEIGHBORS[k]);
+                    if (canCellBeIncluded(map, cellToBeVisited)) stack.push(cellToBeVisited);
+                }
+            }
+        }
+    }
+
+    private boolean canCellBeIncluded(int[][] map, Cell cell) {
         final int xLength = map.length;
         final int yLength = map[0].length;
 
-        return (row >= 0) && (row < xLength) && (col >= 0) && (col < yLength) && (map[row][col] == 1 && !visited[row][col]);
-    }
-
-    private void dfs(int[][] map, int row, int col, boolean visited[][]) {
-        visited[row][col] = true;
-
-        for (int k = 0; k < 8; ++k) {
-            if (canCellBeIncluded(map, row + ROW_NEIGHBORS[k], col + COL_NEIGHBORS[k], visited))
-                dfs(map, row + ROW_NEIGHBORS[k], col + COL_NEIGHBORS[k], visited);
-        }
+        return cell.getRow() >= 0 && cell.getRow() < xLength && cell.getCol() >= 0 && cell.getCol() < yLength
+                && map[cell.getRow()][cell.getCol()] == 1;
     }
 }
